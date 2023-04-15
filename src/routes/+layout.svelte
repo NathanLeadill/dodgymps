3aba1f605e9ac036fe589373f61140918afaaa8a
Nathan Leadill <nathan.l@snowfalltravel.com>
2023-04-15 19:26:48 -0400

    Push for demo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,14 @@
 <script>
-	import Header from './Header.svelte';
 	import './styles.css';
 </script>
 
 <div class="app">
-	<Header />
-
 	<main>
 		<slot />
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		<p>Developed and Managed by Nathan</p>
 	</footer>
 </div>
 
