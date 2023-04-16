e4f161e4e8eea2aabd08eee3a3afd98a27a37a66
Nathan Leadill <nathan.l@snowfalltravel.com>
2023-04-16 15:34:03 -0400

    Information report

---
+++ src/routes/report/+page.svelte
@@ -0,0 +1,15 @@
+<form name="information-report" method="POST" data-netlify="true">
+	<label for="name">Offender Name</label>
+	<input type="text" name="name" />
+
+	<label for="party">Offender Party</label>
+	<input type="text" name="party" />
+
+	<label for="news">Link to news article</label>
+	<input type="url" name="news" />
+
+	<label for="message">Extra information</label>
+	<textarea name="message" />
+
+	<button type="submit">Submit</button>
+</form>
