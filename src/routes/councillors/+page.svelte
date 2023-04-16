1ae50e32c8e841a170d10771f6b3eeaef22b2145
Nathan Leadill <nathan.l@snowfalltravel.com>
2023-04-16 16:37:49 -0400

    Add adverts

---
+++ src/routes/councillors/+page.svelte
@@ -0,0 +1,27 @@
+<ul>
+	<li>Councillor Shaz Saleem - Dudly - Conservative - Sexual Assault</li>
+	<li>Councillor David Smith - Middlesbrough - Repeated Child Sexual Assault & Grooming</li>
+	<li>
+		Councillor Alan Marsh - Herne and Sturry - Conservative - Sexual Harrasment, Misogynist <sub
+			>Reports that he also made repeated derogatory comments towards women in the chamber
+		</sub>
+	</li>
+	<li>
+		Mayor & Councillor John Humphreys - East Devon - Conservative - 2 x Sexual Assault <sub
+			>21 years in prison</sub
+		>
+	</li>
+	<li>
+		Councillor Michael Green - Glenrothes North, Leslie and Markinch - Conservative - 2x Child
+		Sexual Abuse
+	</li>
+	<li>Councillor Alexis McEvoy - Hampshire - Conservative - Racist</li>
+	<li>
+		Councillor Andrew Edwards - Prembrokeshire - Conservative - Racist <sub
+			>"White men should have a Black slaves"</sub
+		>
+	</li>
+	<li>
+		Councillor Richard Bingley - Plymouth - Conservative - Cutting down 110 trees in the city centre
+	</li>
+</ul>
